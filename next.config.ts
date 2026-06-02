6432a14bbab6d3c9e4b69d787ccdf9a7f3b828ab
Rommely Alejandro <rommelyalejandro@gmail.com>
2026-06-02 01:30:49 -0400

    Add rewrite rule to support custom auth domain for Firebase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://clarito-app-d58e1.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
